c8e2d83f8528ac48e69384df860d81206cbfab3d
Jan Tlamicha <shadow.to.root@gmail.com>
2023-04-10 00:34:03 +0200

    Progress

--- include/inercial_nav.h
+++ include/inercial_nav.h
@@ -1,7 +1,77 @@
 #include <config.h>
+#include <Wire.h>
+#include <MPU6050_light.h>
 
 #ifdef MPU_6050_USE
 
+/**
+ * @brief Inercial Navigation class for MPU6050 and logging into Serial.
+ * 
+*/
+class InercialNav{
+    public:
+        InercialNav(int update_rate_ms=500): mpu(Wire), update_rate_ms(update_rate_ms){};
+        void setup_hook();
+        void loop_hook();
 
+
+    private:
+        MPU6050 mpu;
+        float current_rotation_pitch;
+        float timer;
+        float current_angle_x;
+        float previous_angle_x;
+        float previous_timer;
+        int update_rate_ms;
+};
+
+void InercialNav::setup_hook(){
+    Wire.begin();
+    //not much sense, but it's init ... I'll leave it for now ... It's in constructor.
+    MPU6050 mpu(Wire);
+
+    long timer = 0;
+    
+    byte status = mpu.begin();
+    Serial.print(F("MPU6050 status: "));
+    Serial.println(status);
+    while(status!=0){ } // stop everything if could not connect to MPU6050
+    
+    Serial.println(F("Calculating offsets, do not move MPU6050"));
+    delay(1000);
+    mpu.calcOffsets(true,true); // gyro and accelero
+    Serial.println("Done!\n");
+
+}
+
+void InercialNav::loop_hook(){
+  mpu.update();
+
+  if(millis() - timer > update_rate_ms){ // update based on update_rate_ms
+    Serial.print(F("TEMPERATURE: "));Serial.println(mpu.getTemp());
+    Serial.print(F("ACCELERO  X: "));Serial.print(mpu.getAccX());
+    Serial.print("\tY: ");Serial.print(mpu.getAccY());
+    Serial.print("\tZ: ");Serial.println(mpu.getAccZ());
+  
+    Serial.print(F("GYRO      X: "));Serial.print(mpu.getGyroX());
+    Serial.print("\tY: ");Serial.print(mpu.getGyroY());
+    Serial.print("\tZ: ");Serial.println(mpu.getGyroZ());
+  
+    Serial.print(F("ACC ANGLE X: "));Serial.print(mpu.getAccAngleX());
+    Serial.print("\tY: ");Serial.println(mpu.getAccAngleY());
+    
+    //Angles in degrees
+    previous_timer = timer;
+    previous_angle_x = current_angle_x;
+    current_angle_x = mpu.getAngleX();
+
+    Serial.print(F("ANGLE     X: "));Serial.print(current_angle_x);
+    Serial.print("\tY: ");Serial.print(mpu.getAngleY());
+    Serial.print("\tZ: ");Serial.println(mpu.getAngleZ());
+    Serial.println(F("=====================================================\n"));
+    timer = millis();
+  }
+
+}
 
 #endif
--- include/motor.h
+++ include/motor.h
@@ -7,9 +7,13 @@
 
 #ifdef MOTOR_SHIELD_USE
 
+/**
+ * @brief Motor class for left and right motor for movement.
+ * 
+*/
 class LRMotor {
 public:
-  LRMotor(const int left_motor, const int right_motor): motor_left(left_motor), motor_right(right_motor) {}
+  LRMotor(const int left_motor_channel=1, const int right_motor_channel=2): motor_left(left_motor_channel), motor_right(right_motor_channel) {}
   
   void motor_setup_hook(){
     motor_stop();
@@ -49,6 +53,46 @@ public:
 private:
   AF_DCMotor motor_left;
   AF_DCMotor motor_right;
-}
+};
+
+class BrushMotor{
+  public:
+    BrushMotor(const int motor_channel=3): brushMotor(motor_channel) {}
+    
+    void motor_setup_hook(){
+      stop();
+    }
+    
+    void brush(int speed=255){
+      brushMotor.setSpeed(speed);
+      brushMotor.run(FORWARD);
+    }
+
+    void stop(){
+      brushMotor.run(RELEASE);
+    }
+  private:
+    AF_DCMotor brushMotor;
+};
+
+class HoverMotor{
+  public:
+    HoverMotor(const int motor_channel=4): hoverMotor(motor_channel) {}
+    
+    void motor_setup_hook(){
+      hover_stop();
+    }
+    
+    void hover(int speed=255){
+      hoverMotor.setSpeed(speed);
+      hoverMotor.run(FORWARD);
+    }
+
+    void hover_stop(){
+      hoverMotor.run(RELEASE);
+    }
+  private:
+    AF_DCMotor hoverMotor;
+};
 
 #endif
